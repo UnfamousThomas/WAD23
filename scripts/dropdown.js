fad767739e3367586e1cb4ee8d6d303ed0738326
madis025 <77383233+madis025@users.noreply.github.com>
2023-11-05 09:43:46 +0200

    Dropdown

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,20 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const profiilipildid = document.getElementsByClassName("profiilikas_navi");
+    const profiilipildid = document.getElementsByClassName("navigeerimisePilt");
     for (let profiilipilt of profiilipildid) {
         profiilipilt.addEventListener('click', () => {
-            const pildiVanem = profiilipilt.parentElement;
-            const uusDiv = document.createElement('div');
-            const uusTekst1 = document.createElement('a');
-            const uusTekst2 = document.createElement('a');
-            const uusTekst3 = document.createElement('a');
-            uusTekst1.innerText = "John Doe\n";
-            uusTekst2.innerText = "john.doe@ut.ee\n";
-            uusTekst3.innerText = "logout";
-            uusDiv.appendChild(uusTekst1);
-            uusDiv.appendChild(uusTekst2);
-            uusDiv.appendChild(uusTekst3);
-            uusDiv.className = "profiilikaDropdown-active";
-            pildiVanem.appendChild(uusDiv);
+            const pildiVanem = profiilipilt.parentNode;
+            if (pildiVanem.children.length === 1) {
+                const uusDiv = document.createElement('div');
+                const uusTekst1 = document.createElement('a');
+                const uusTekst2 = document.createElement('a');
+                const uusTekst3 = document.createElement('a');
+                uusTekst1.innerText = "John Doe\n";
+                uusTekst2.innerText = "john.doe@ut.ee\n";
+                uusTekst3.innerText = "logout";
+                uusDiv.appendChild(uusTekst1);
+                uusDiv.appendChild(uusTekst2);
+                uusDiv.appendChild(uusTekst3);
+                uusDiv.className = "profiilikaDropdown-active";
+                pildiVanem.appendChild(uusDiv);
+            } else {
+                for (let activeDropdown of document.getElementsByClassName("profiilikaDropdown-active")) {
+                    activeDropdown.remove();
+                }
+            }
         })
     }
 })
