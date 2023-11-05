e9abd0c07f8c02c40c609637938324e55b72f426
Karl Mattias Pärloja <karlmattiasparloja@gmail.com>
2023-11-05 15:51:15 +0200

    Added dropdown functionality

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,26 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const profiilipildid = document.getElementsByClassName("navigeerimisePilt");
-    for (let profiilipilt of profiilipildid) {
-        profiilipilt.addEventListener('click', () => {
-            const pildiVanem = profiilipilt.parentNode;
-            if (pildiVanem.children.length === 1) {
-                const uusDiv = document.createElement('div');
-                const uusTekst1 = document.createElement('a');
-                const uusTekst2 = document.createElement('a');
-                const uusTekst3 = document.createElement('a');
-                uusTekst1.innerText = "John Doe\n";
-                uusTekst2.innerText = "john.doe@ut.ee\n";
-                uusTekst3.innerText = "logout";
-                uusDiv.appendChild(uusTekst1);
-                uusDiv.appendChild(uusTekst2);
-                uusDiv.appendChild(uusTekst3);
-                uusDiv.className = "profiilikaDropdown-active";
-                pildiVanem.appendChild(uusDiv);
-            } else {
-                for (let activeDropdown of document.getElementsByClassName("profiilikaDropdown-active")) {
-                    activeDropdown.remove();
-                }
-            }
-        })
-    }
-})
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var dropdownContent = document.createElement("div");
+    dropdownContent.className = "dropdown";
+    dropdownContent.innerHTML = "<p>John Doe</p><p>john.doe@ut.ee</p><p id='logout'>Logout</p>";
+
+    var profileImage = document.getElementById("profileImage");
+
+    profileImage.parentNode.appendChild(dropdownContent);
+
+    profileImage.addEventListener("click", function(event) {
+        var dropdown = document.querySelector(".dropdown");
+        dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+        
+        event.stopPropagation();
+    });
+
+    document.addEventListener("click", function() {
+        var dropdown = document.querySelector(".dropdown");
+        dropdown.style.display = "none";
+    });
+
+    document.getElementById("logout").addEventListener("click", function() {
+        // Lisame hiljem
+        alert("Logout clicked!");
+    });
+});
